6d8cb7d1808b5965e2aa3b8b4fbbf96e5e4e04dc
curiousdenis <torooopov99@gmail.com>
2024-12-16 12:41:47 +0700

    Allure files

--- conftest.py
+++ conftest.py
@@ -1,13 +1,16 @@
+import allure
 import pytest
 from playwright.sync_api import sync_playwright, expect, Page
 from HomePage import HomePage
+
 
 @pytest.fixture(scope='function')
 def chronium_page() -> Page:
     "Inititialize page"
     with sync_playwright() as playwright:
         chronium = playwright.chromium.launch(headless=True)
-        yield chronium.new_page()
+        context = chronium.new_context(record_video_dir="allure_results/")
+        yield context.new_page()
 
 @pytest.fixture(scope='function')
 def playwright_chrome_page(chronium_page: Page) -> HomePage:
@@ -25,4 +28,16 @@ def common_data() -> dict:
 
 @pytest.fixture(scope='function')
 def benchmark_data():
+    "For the benchmark backend testing"
     pass
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call = 'call'):
+    "To generate image and video for failed tests"
+    report = yield
+    page = item.funcargs['chronium_page']
+    if report.get_result().outcome == 'failed':
+        allure.attach(page.screenshot(), name = 'Screenshot_error', attachment_type= allure.attachment_type.PNG)
+        video_path = page.video.path()
+        page.context.close()
+        allure.attach(open(video_path, 'rb').read(),name=f"Video_error", attachment_type=allure.attachment_type.WEBM)
