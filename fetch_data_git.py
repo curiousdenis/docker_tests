f04ba049a8251319b3f9d8bb29f35a7674c828ec
curiousdenis <torooopov99@gmail.com>
2024-12-19 20:15:05 +0700

    finall changes

--- fetch_data_git.py
+++ fetch_data_git.py
@@ -1,8 +1,7 @@
 import requests
 import sys
-import os
 
-def download_url_last_result(TOKEN: str, owner: str, repo: str, to_fetch: str) -> None:
+def download_url_last_result(TOKEN: str, owner: str, repo: str, to_fetch: str) -> str:
     "This function will be used to fetch id of the last specific artifact to generate allure report"
     curl = f"https://api.github.com/repos/{owner}/{repo}/actions/artifacts"
     headers = {
@@ -12,14 +11,11 @@ def download_url_last_result(TOKEN: str, owner: str, repo: str, to_fetch: str) -
     }
     r = requests.get(curl + f"?name={to_fetch}", headers=headers)
     data = r.json()
-    env_file = os.getenv('GITHUB_ENV')
     if r.status_code == 200:
-        with open(env_file, "a") as myenv:
-            if data["total_count"] != 0:
-                myenv.write(f"DOWNLOAD_LINK={data['artifacts'][0]['archive_download_url']}")
-                raise ValueError(f'{os.environ["DOWNLOAD_LINK"]}')
-            else:
-                myenv.write(f"DOWNLOAD_LINK=EMPTY")
+        if data["total_count"] != 0:
+            return data['artifacts'][0]['archive_download_url']
+        else:
+            return "EMPTY"
     else:
         raise ConnectionError(f'Status connection is off for your spec: owner = {owner}, repo = {repo}, fetch = {to_fetch}\n'
                               f'Your data = {data}\n'
